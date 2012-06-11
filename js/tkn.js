ecd97e8da645b8651932e07b1fa04236e4232d0d
mustefa <mail@mustefa.com>
2012-06-10 20:18:38 -0400

    summary

--- js/tkn.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$(function () {
-		$(".btn").bind('click', function () {
- 			span = "." + $(this).parent().find("input").attr("id")
-			$(span).text(" " + $(this).parent().find('input:text').val())
-		
-		});
-	});
-});
